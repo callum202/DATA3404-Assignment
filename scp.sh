1ab645d44a5c10b1f4450628d85f4b52d5196757
callum202 <31586495+callum202@users.noreply.github.com>
2018-06-01 02:13:45 +1000

    Handling varying arg counts and option for default or custom outputfile name
    The script now handles varying arg counts and allows for the option to specify a custom name for the output file. If no custom name is specified, the output file will be named "[TaskName]([flightDataDir]).txt". For example, TopThreeAirports(tiny).txt

--- scp.sh
+++ scp.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 #run ./scp build [Task Number] to run the build script.
+#CHANGE LINE 8 TO REFLECT YOUR FOLDER SYSTEM
 if [ $1 == "build" ]
 then
   task=$2
-  cd Task\ $task #CHANGE THIS LINE TO REFLECT YOUR FOLDER SYSTEM
+  cd Task\ $task
   ./build.sh
 
 #run ./scp [unikey] scp to transfer this script to the ssh
@@ -12,7 +13,17 @@ elif [ $1 == "scp" ]
 then
   scp scp.sh $2@soit-hdp-pro-14.ucc.usyd.edu.au:/home/$2/
 
-#run ./scp [Task Number] [Arg0] [Arg1] [Arg2] [Arg2] to run the jar on the cluster
+# run ./scp [Task Number] [Arg0] [Arg1] [Arg2] [Arg3](Optional) to run the jar on the cluster, optionally specifying a file name for the output file
+# If [Task Number] is 3, and,
+    # 3 args have been input: Task 3 will be run with args: [arg0](flightDataDir) [arg1](unikey)
+      # and the output file name will default to MostPopularAircraftTypes([arg0]).txt
+    # 4 args have been input: Task 3 will be run with 3 args: [arg0](flightDataDir) [arg1](unikey) [arg2](outputFileName)
+      # and the output file name will be a custom name specified in [arg2]
+# If [Task Number] is 1 or 2, and,
+    # 4 args have been input, the task will be run with args: [arg0](year) [arg1](flightDataDir) [arg2](unikey)
+      # and the output file name will default to [TaskName]([arg0]).txt
+    # 5 args have been input, the task will be run with args: [arg0](year) [arg1](flightDataDir) [arg2](unikey) [arg3](outputFileName)
+      # and the output file name will be a custom name specified in [arg3]
 else
   task=$1
   arg0=$2
@@ -27,5 +38,21 @@ else
   esac
 
 
-  $FLINK_HOME/bin/flink run -m yarn-cluster -yn 2 --class $class output$task.jar $arg0 $arg1 $arg2 $arg3
+  if [ "$#" -eq 3 ]
+  then
+    $FLINK_HOME/bin/flink run -m yarn-cluster -yn 2 --class $class output$task.jar $arg0 $class"("$arg0")" $arg1
+  elif [ "$#" -eq 4 ]
+  then
+    if [ $task -eq 3 ]
+    then
+      $FLINK_HOME/bin/flink run -m yarn-cluster -yn 2 --class $class output$task.jar $arg0 $arg2 $arg1
+    else
+      $FLINK_HOME/bin/flink run -m yarn-cluster -yn 2 --class $class output$task.jar $arg0 $arg1 $class"("$arg1")" $arg2
+    fi
+  elif [ "$#" -eq 5 ]
+  then
+    $FLINK_HOME/bin/flink run -m yarn-cluster -yn 2 --class $class output$task.jar $arg0 $arg1 $arg3 $arg2
+  else
+    exit
+  fi
 fi
