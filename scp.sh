332f8eff3e3804f53492b5446be890918a9b460b
callum202 <31586495+callum202@users.noreply.github.com>
2018-06-01 00:17:47 +1000

    Add files via upload

---
+++ scp.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#run ./scp build [Task Number] to run the build script.
+#CHANGE LINE 8 TO REFLECT YOUR FOLDER SYSTEM
+if [ $1 == "build" ]
+then
+  task=$2
+  cd Task\ $task
+  ./build.sh
+
+#run ./scp [unikey] scp to transfer this script to the ssh
+elif [ $1 == "scp" ]
+then
+  scp scp.sh $2@soit-hdp-pro-14.ucc.usyd.edu.au:/home/$2/
+
+#run ./scp [Task Number] [Arg0] [Arg1] [Arg2] [Arg2] to run the jar on the cluster
+else
+  task=$1
+  arg0=$2
+  arg1=$3
+  arg2=$4
+  arg3=$5
+
+  case $task in
+    1) class="TopThreeAirports";;
+    2) class="AverageFlightDelay";;
+    3) class="MostPopularAircraftTypes";;
+  esac
+
+
+  $FLINK_HOME/bin/flink run -m yarn-cluster -yn 2 --class $class output$task.jar $arg0 $arg1 $arg2 $arg3
+fi
